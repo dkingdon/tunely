049b85753b50ed43db166e2c63a9e6ed03f859d8
dkingdon <daniel.kingdon@gmail.com>
2016-11-14 16:30:29 -0800

    sprint 1 step 3, creating db, album schema, export and require mongoose code

--- models/album.js
+++ models/album.js
@@ -1,2 +1,13 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+
+  var AlblumSchema = new Schema({
+    artistName: String,
+    name: String,
+    releaseDate: String,
+    genres: [String]
+  });
+
+var Album = mongoose.model('Frame', FrameSchema);
+
+model.export = Frame;
