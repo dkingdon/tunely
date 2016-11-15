a03e379a81ecfb2d8b43953111e6dc13475b2acc
dkingdon <daniel.kingdon@gmail.com>
2016-11-14 16:20:39 -0800

    sprint 1 step two complete. added index route on server

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,31 +2,31 @@
 
 
 /* hard-coded data! */
-var sampleAlbums = [];
-sampleAlbums.push({
-             artistName: 'Ladyhawke',
-             name: 'Ladyhawke',
-             releaseDate: '2008, November 18',
-             genres: [ 'new wave', 'indie rock', 'synth pop' ]
-           });
-sampleAlbums.push({
-             artistName: 'The Knife',
-             name: 'Silent Shout',
-             releaseDate: '2006, February 17',
-             genres: [ 'synth pop', 'electronica', 'experimental' ]
-           });
-sampleAlbums.push({
-             artistName: 'Juno Reactor',
-             name: 'Shango',
-             releaseDate: '2000, October 9',
-             genres: [ 'electronic', 'goa trance', 'tribal house' ]
-           });
-sampleAlbums.push({
-             artistName: 'Philip Wesley',
-             name: 'Dark Night of the Soul',
-             releaseDate: '2008, September 12',
-             genres: [ 'piano' ]
-           });
+// var sampleAlbums = [];
+// sampleAlbums.push({
+//              artistName: 'Ladyhawke',
+//              name: 'Ladyhawke',
+//              releaseDate: '2008, November 18',
+//              genres: [ 'new wave', 'indie rock', 'synth pop' ]
+//            });
+// sampleAlbums.push({
+//              artistName: 'The Knife',
+//              name: 'Silent Shout',
+//              releaseDate: '2006, February 17',
+//              genres: [ 'synth pop', 'electronica', 'experimental' ]
+//            });
+// sampleAlbums.push({
+//              artistName: 'Juno Reactor',
+//              name: 'Shango',
+//              releaseDate: '2000, October 9',
+//              genres: [ 'electronic', 'goa trance', 'tribal house' ]
+//            });
+// sampleAlbums.push({
+//              artistName: 'Philip Wesley',
+//              name: 'Dark Night of the Soul',
+//              releaseDate: '2008, September 12',
+//              genres: [ 'piano' ]
+//            });
 /* end of hard-coded data */
 
 
@@ -34,24 +34,40 @@ sampleAlbums.push({
 
 $(document).ready(function() {
   console.log('app.js loaded!');
+
+  $.get({
+    method: 'GET',
+    url: '/api/albums',
+    type: 'json',
+    success: albumGetSuccess,
+    error: function( err ){
+      console.log("there was an error")
+    }
+  })
+
   // $.get('/api/albums').success(function (albums) {
   //   albums.forEach(function (album) {
   //     renderAlbum(album);
   //   })
   // })
-    sampleAlbums.forEach(function (e) {
-      renderAlbum(e);
-  });
+  //   sampleAlbums.forEach(function (e) {
+  //     renderAlbum(e);
+  // });
 
 
 
 
 }); //END OF - document ready function
 
+function albumGetSuccess (json){
+  var receivedAlbums = json.albums;
+  console.log(receivedAlbums);
+  receivedAlbums.forEach(function renderOneAlbum (album) {
+    renderAlbum(album);
+  });
+}
 
-
-
-// this function takes a single album and renders it to the page
+  // this function takes a single album and renders it to the page
 function renderAlbum(album) {
   /* - - - Handlebars - - - */
   var source = $('#hb-template-script').html(); // Pulling html structure
